f116c7dc7b917550ad5406e177dfbcfc8493cc72
molarmanful <molarmanful@users.noreply.github.com>
2014-11-11 13:58:06 -0800

    Update googlecubeembed.js

--- googlecubeembed.js
+++ googlecubeembed.js
@@ -360,7 +360,7 @@
 						$(this).text('Revert to Previous State');
 						cube.twist(settings.algorithm.replace(/[]()\[\]{}]/g, '').trim().split(' ').forEach(function(i){
 							if(i.match(/[i`']/gi)){
-								i = i.toLowercase();
+								i = i.toLowerCase();
 							}
 							if(i.match(/[2]/i)){
 								i += i;
@@ -370,7 +370,7 @@
 						$(this).text('Play Algorithm');
 						cube.twist(settings.algorithm.reverse().replace(/[\[\](){}]/g, '').trim().split(' ').forEach(function(i){
 							if(i.match(/[i`']/gi)){
-								i = i.toLowercase();
+								i = i.toLowerCase();
 							}
 							if(i.match(/[2]/i)){
 								i += i;
