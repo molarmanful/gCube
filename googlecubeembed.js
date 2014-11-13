78c36c07a04e7bdae1158d239f862ee8af9c1bf7
molarmanful <molarmanful@users.noreply.github.com>
2014-11-13 10:01:26 -0800

    Update googlecubeembed.js

--- googlecubeembed.js
+++ googlecubeembed.js
@@ -329,7 +329,7 @@
 		var settings = $.extend({
 			speed: 1000,
 			scramble: null,
-			initcontrols: true,
+			initcontrols: false,
 			algorithm: null
 		}, options);
 		this.filter('g-cube').each(function(e){
@@ -340,10 +340,8 @@
 				'display': 'block'
 			});
 			window.cube = new ERNO.Cube();
-			if(settings.speed != 1000 && typeof settings.speed == 'number'){
-				cube.twistDuration = settings.speed;
-			}
-			if(settings.scramble){
+			cube.twistDuration = settings.speed;
+			if(settings.scramble != null){
 				cube.twistDuration = 10;
 				stn = settings.scramble.replace(/[]()\[\]{}]/g, '').split(' ').forEach(function(i){
 					if(i.match(/[i`']/gi)){
@@ -356,7 +354,7 @@
 				cube.twist(stn);
 				cube.twistDuration = settings.speed;
 			}
-			if(settings.initcontrols && settings.initcontrols === true && settings.algorithm){
+			if(settings.initcontrols === true && settings.algorithm){
 				$('g-cube').append('<button id="playalg" style="top: 0; left: 0; position: absolute; z-index: 100">Play Algorithm</button>');
 				$('g-cube #playalg').click(function(){
 					if($(this).text() == 'Play Algorithm'){
