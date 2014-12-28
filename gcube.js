b199c6040cafcd6f129ef03e8ea553c2ad7588cf
molarmanful <molarmanful@users.noreply.github.com>
2014-12-27 16:48:22 -0800

    Update gcube.js

--- gcube.js
+++ gcube.js
@@ -397,7 +397,7 @@ function reverse(s){
 						}
 					});
 				}
-				$(this).append('<button id="playalg" style="position: absolute; top: 0; clear: both; z-index: 100">Play Algorithm</button>');
+				$(this).append('<button id="playalg" style="position: absolute; top: 0; float: right; z-index: 100">Play Algorithm</button>');
 				$(this).children('#playalg').click(function(){
 					if($(this).text() == 'Play Algorithm'){
 						cube.twistDuration = settings.speed;
