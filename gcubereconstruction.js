cbc6bd5a6a42f25752a95cf7959ba22b97bdb24a
molarmanful <molarmanful@users.noreply.github.com>
2015-01-09 10:04:32 -0800

    Update gcubereconstruction.js

--- gcubereconstruction.js
+++ gcubereconstruction.js
@@ -1,1 +1,13 @@
-
+(function( $ ) {
+	$.fn.gce = function(options, callback) {
+		
+		//only <g-cube></g-cube> allowed
+		this.filter('g-cube').each(function(){
+		  
+		});
+		
+		//debug purposes
+		console.log('gCube reconstruction loaded.');
+		return this;
+	};
+}( jQuery ));
