60d9618b822bda8e3ed4109e6241ab6eb5906525
molarmanful <molarmanful@users.noreply.github.com>
2015-01-12 10:10:25 -0800

    Update gcubereconstruction.js

--- gcubereconstruction.js
+++ gcubereconstruction.js
@@ -1,3 +1,5 @@
+//This plugin requires gcube.js and jQuery to run.
+
 (function( $ ) {
 	//plugin start
 	$.fn.reconstruct = function(algs, comments, callback) {
@@ -5,7 +7,7 @@
 		//only <g-cube></g-cube> allowed
 		this.filter('g-cube').each(function(){
 			//only allow arrays of equal length for params
-			if(typeof alg == 'array' && typeof comments == 'array' && algs.length == comments.length){
+			if(typeof algs == 'array' && typeof comments == 'array' && algs.length == comments.length){
 				//check for cube presence
 				if(window.cube){
 					rec();
@@ -15,13 +17,18 @@
 			//reconstruct function - still can make 'this' refer to a g-cube tag
 			function rec(){
 				//overwrite any buttons previously in tag except for cube container and speed slider with reconstruction buttons
-				$(this).children(':not(.cubecont):not(.speedslider)').remove().prepend('<button class="rec" style="top: 0; z-index: 100">Play Reconstruction</button><br><span id="steps"></span><br>');
-				//play button
+				$(this).children(':not(.cubecont):not(.speedslider)').remove().prepend('<button class="rec" style="top: 0; z-index: 100">Play Reconstruction</button><br><span class="steps"></span><br>');
+				//play button clicked
 				$('.rec').click(function(){
 					//play through steps
 					var count = 0;
-					while(count <= alg.length){
-						
+					while(count < algs.length){
+						//comments are where you display your algs and comments (duh!)
+						$('.steps').html(comments);
+						//twist alg
+						cube.twist(algs[count]);
+						//no infinite loops!
+						count++;
 					}
 				});
 			}
@@ -33,7 +40,7 @@
 		});
 		
 		//debug purposes
-		console.log('gCube reconstruction loaded.');
+		console.log('gCube solve reconstruction loaded.');
 		
 		//chaining
 		return this;
