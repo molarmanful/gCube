062b099d6a217812e5d3ffcf5704fb25f3b5d1df
molarmanful <molarmanful@users.noreply.github.com>
2015-03-18 19:30:49 -0700

    Update gcube.js

--- gcube.js
+++ gcube.js
@@ -181,21 +181,21 @@ var scram, algo, algor;
 			}
 			else if(x.match('oll') && !(x.match('eoll')) && !(x.match('coll'))){
 				cube.showStickers();
-				$(this).children('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');
+				$(this).find('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');
 			}
 			else if(x.match('eoll')){
 				cube.showStickers();
 				cube.up.corners.hideStickers();
-				$(this).children('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
+				$(this).find('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
 			}
 			else if(x.match('ocll')){
 				cube.showStickers();
 				cube.up.edges.hideStickers();
-				$(this).children('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
+				$(this).find('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
 			}
 			else if(x.match('coll')){
 				cube.showStickers();
-				$(this).children('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
+				$(this).find('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
 				cube.up.corners.showStickers();
 			}
 			else if(x.match('2x2x2')){
@@ -386,21 +386,21 @@ var scram, algo, algor;
 			}
 			else if(x.match('oll') && !(x.match('eoll')) && !(x.match('coll'))){
 				cube.showStickers();
-				$(this).children('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
+				$(this).find('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
 			}
 			else if(x.match('eoll')){
 				cube.showStickers();
 				cube.up.corners.hideStickers();
-				$(this).children('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
+				$(this).find('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
 			}
 			else if(x.match('ocll')){
 				cube.showStickers();
 				cube.up.edges.hideStickers();
-				$(this).children('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
+				$(this).find('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
 			}
 			else if(x.match('coll')){
 				cube.showStickers();
-				$(this).children('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
+				$(this).find('.cubeletId-0 .faceLeft .sticker, .cubeletId-0 .faceFront .sticker, .cubeletId-1 .faceFront .sticker, .cubeletId-2 .faceFront .sticker, .cubeletId-2 .faceRight .sticker, .cubeletId-11 .faceRight .sticker, .cubeletId-20 .faceRight .sticker, .cubeletId-20 .faceBack .sticker, .cubeletId-19 .faceBack .sticker, .cubeletId-18 .faceBack .sticker, .cubeletId-18 .faceLeft .sticker, .cubeletId-9 .faceLeft .sticker').css('display', 'none');			
 				cube.up.corners.showStickers();
 			}
 			else if(x.match('2x2x2')){
