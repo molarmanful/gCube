a67e8b82c23e29d8672c414c36713ce4ee19f097
molarmanful <molarmanful@users.noreply.github.com>
2015-03-18 17:28:42 -0700

    Update gcube.js

--- gcube.js
+++ gcube.js
@@ -175,28 +175,45 @@ var scram, algo, algor;
 			else if(x.match('f2l')){
 				cube.showStickers();
 				cube.up.hideStickers();
+				cube.centers.showStickers();
 				cube.rotation.x = 100;
 			}
-			else if(x.match('oll') && !(x.match('eoll')) && !(x.match('ocll'))){
+			else if(x.match('oll') && !(x.match('eoll')) && !(x.match('coll'))){
+				cube.down.showStickers();
+				cube.equator.showStickers();
 				$('.cubeletId-0 .faceLeft, .cubeletId-0 .faceFront, .cubeletId-1 .faceFront, .cubeletId-2 .faceFront, .cubeletId-2 .faceRight, .cubeletId-11 .faceRight, .cubeletId-20 .faceRight, .cubeletId-20 .faceBack, .cubeletId-19 .faceBack, .cubeletId-18 .faceBack, .cubeletId-18 .faceLeft, .cubeletId-9 .faceLeft').children('.sticker').css('display', 'none');
 			}
 			else if(x.match('eoll')){
+				cube.down.showStickers();
+				cube.equator.showStickers();
+				cube.up.corners.hideStickers();
 				$('.cubeletId-0 .faceLeft, .cubeletId-0 .faceFront, .cubeletId-1 .faceFront, .cubeletId-2 .faceFront, .cubeletId-2 .faceRight, .cubeletId-11 .faceRight, .cubeletId-20 .faceRight, .cubeletId-20 .faceBack, .cubeletId-19 .faceBack, .cubeletId-18 .faceBack, .cubeletId-18 .faceLeft, .cubeletId-9 .faceLeft').children('.sticker').css('display', 'none');
 			}
 			else if(x.match('ocll')){
+				cube.down.showStickers();
+				cube.equator.showStickers();
+				cube.up.edges.hideStickers();
 				$('.cubeletId-0 .faceLeft, .cubeletId-0 .faceFront, .cubeletId-1 .faceFront, .cubeletId-2 .faceFront, .cubeletId-2 .faceRight, .cubeletId-11 .faceRight, .cubeletId-20 .faceRight, .cubeletId-20 .faceBack, .cubeletId-19 .faceBack, .cubeletId-18 .faceBack, .cubeletId-18 .faceLeft, .cubeletId-9 .faceLeft').children('.sticker').css('display', 'none');
+			}
+			else if(x.match('coll')){
+				cube.down.showStickers();
+				cube.equator.showStickers();
+				$('.cubeletId-0 .faceLeft, .cubeletId-0 .faceFront, .cubeletId-1 .faceFront, .cubeletId-2 .faceFront, .cubeletId-2 .faceRight, .cubeletId-11 .faceRight, .cubeletId-20 .faceRight, .cubeletId-20 .faceBack, .cubeletId-19 .faceBack, .cubeletId-18 .faceBack, .cubeletId-18 .faceLeft, .cubeletId-9 .faceLeft').children('.sticker').css('display', 'none');
+				cube.up.corners.showStickers();
 			}
 			else if(x.match('2x2x2')){
 				cube.showStickers();
 				cube.up.hideStickers();
 				cube.back.hideStickers();
 				cube.left.hideStickers();
+				cube.centers.showStickers();
 				cube.rotation.x = 100;
 			}
 			else if(x.match('2x2x3')){
 				cube.showStickers();
 				cube.up.hideStickers();
 				cube.back.hideStickers();
+				cube.centers.showStickers();
 				cube.rotation.x = 100;
 			}
 			else if(x.match('eoline')){
@@ -204,6 +221,7 @@ var scram, algo, algor;
 				cube.up.hideStickers();
 				cube.left.hideStickers();
 				cube.right.hideStickers();
+				cube.centers.showStickers();
 				cube.rotation.x = 100;
 			}
 			else if(x.match('fb')){
@@ -365,28 +383,45 @@ var scram, algo, algor;
 			else if(x.match('f2l')){
 				cube.showStickers();
 				cube.up.hideStickers();
+				cube.centers.showStickers();
 				cube.rotation.x = 100;
 			}
-			else if(x.match('oll') && !(x.match('eoll')) && !(x.match('ocll'))){
+			else if(x.match('oll') && !(x.match('eoll')) && !(x.match('coll'))){
+				cube.down.showStickers();
+				cube.equator.showStickers();
 				$('.cubeletId-0 .faceLeft, .cubeletId-0 .faceFront, .cubeletId-1 .faceFront, .cubeletId-2 .faceFront, .cubeletId-2 .faceRight, .cubeletId-11 .faceRight, .cubeletId-20 .faceRight, .cubeletId-20 .faceBack, .cubeletId-19 .faceBack, .cubeletId-18 .faceBack, .cubeletId-18 .faceLeft, .cubeletId-9 .faceLeft').children('.sticker').css('display', 'none');
 			}
 			else if(x.match('eoll')){
+				cube.down.showStickers();
+				cube.equator.showStickers();
+				cube.up.corners.hideStickers();
 				$('.cubeletId-0 .faceLeft, .cubeletId-0 .faceFront, .cubeletId-1 .faceFront, .cubeletId-2 .faceFront, .cubeletId-2 .faceRight, .cubeletId-11 .faceRight, .cubeletId-20 .faceRight, .cubeletId-20 .faceBack, .cubeletId-19 .faceBack, .cubeletId-18 .faceBack, .cubeletId-18 .faceLeft, .cubeletId-9 .faceLeft').children('.sticker').css('display', 'none');
 			}
 			else if(x.match('ocll')){
+				cube.down.showStickers();
+				cube.equator.showStickers();
+				cube.up.edges.hideStickers();
 				$('.cubeletId-0 .faceLeft, .cubeletId-0 .faceFront, .cubeletId-1 .faceFront, .cubeletId-2 .faceFront, .cubeletId-2 .faceRight, .cubeletId-11 .faceRight, .cubeletId-20 .faceRight, .cubeletId-20 .faceBack, .cubeletId-19 .faceBack, .cubeletId-18 .faceBack, .cubeletId-18 .faceLeft, .cubeletId-9 .faceLeft').children('.sticker').css('display', 'none');
+			}
+			else if(x.match('coll')){
+				cube.down.showStickers();
+				cube.equator.showStickers();
+				$('.cubeletId-0 .faceLeft, .cubeletId-0 .faceFront, .cubeletId-1 .faceFront, .cubeletId-2 .faceFront, .cubeletId-2 .faceRight, .cubeletId-11 .faceRight, .cubeletId-20 .faceRight, .cubeletId-20 .faceBack, .cubeletId-19 .faceBack, .cubeletId-18 .faceBack, .cubeletId-18 .faceLeft, .cubeletId-9 .faceLeft').children('.sticker').css('display', 'none');
+				cube.up.corners.showStickers();
 			}
 			else if(x.match('2x2x2')){
 				cube.showStickers();
 				cube.up.hideStickers();
 				cube.back.hideStickers();
 				cube.left.hideStickers();
+				cube.centers.showStickers();
 				cube.rotation.x = 100;
 			}
 			else if(x.match('2x2x3')){
 				cube.showStickers();
 				cube.up.hideStickers();
 				cube.back.hideStickers();
+				cube.centers.showStickers();
 				cube.rotation.x = 100;
 			}
 			else if(x.match('eoline')){
@@ -394,6 +429,7 @@ var scram, algo, algor;
 				cube.up.hideStickers();
 				cube.left.hideStickers();
 				cube.right.hideStickers();
+				cube.centers.showStickers();
 				cube.rotation.x = 100;
 			}
 			else if(x.match('fb')){
