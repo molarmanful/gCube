a410e477b3f53b400137b3f8c5f99532215a8b27
Benjamin Pang <molarmanful@users.noreply.github.com>
2015-05-07 21:10:42 -0700

    Update gcube.js

--- gcube.js
+++ gcube.js
@@ -307,11 +307,13 @@ $(document).ready(function(){
 		$(this).gcube().gspeed(s).gscramble(sc).galgorithm(a).ghighlight(h, function(){
       if(h.match('oll') || h.match('eoll') || h.match('coll')){
         $(this).find('.cubeletId-1, .cubeletId-11, .cubeletId-19, .cubeletId-9').find('.sticker.orange').css('display', 'block');
-        console.log('Edges', $(this).find('.cubeletId-1, .cubeletId-11, .cubeletId-19, .cubeletId-9').find('.sticker.orange').css('display'));
+        console.log('Edges');
+        console.log($(this).find('.cubeletId-1, .cubeletId-11, .cubeletId-19, .cubeletId-9').find('.sticker.orange').css('display'));
     	}
     	if((h.match('oll') || h.match('ocll')) && !(h.match('coll') || h.match('eoll'))){
         $(this).find('.cubeletId-0, .cubeletId-2, .cubeletId-20, .cubeletId-18').find('.sticker.orange').css('display', 'block');
-        console.log('Corners', $(this).find('.cubeletId-0, .cubeletId-2, .cubeletId-20, .cubeletId-18').find('.sticker.orange').css('display'));
+        console.log('Corners');
+        console.log($(this).find('.cubeletId-0, .cubeletId-2, .cubeletId-20, .cubeletId-18').find('.sticker.orange').css('display'));
     	}
 		}).gtext(t).gflorian(f);
 	});
