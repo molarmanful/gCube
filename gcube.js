3aba8d29357ba98441e24cd6b249eb0cf8409f12
Benjamin Pang <molarmanful@users.noreply.github.com>
2015-05-07 21:13:47 -0700

    Update gcube.js

--- gcube.js
+++ gcube.js
@@ -297,7 +297,9 @@ cube.core.setOpacity(0);
 }( jQuery ));
 $(document).ready(function(){
 	$('head').prepend('<link rel="stylesheet" type="text/css" href="https://molarmanful.github.io/MoyuWeilong/cubenologo.css">');
-	$('g-cube').each(function(){
+});
+$(window).load(function(){
+  $('g-cube').each(function(){
 		var s = $(this).find('g-speed').text(), 
 		sc = $(this).find('g-scramble').text(), 
 		a = $(this).find('g-algorithm').text(), 
