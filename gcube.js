fae0100c0b6ff3cad8b563ea4fc9041cbf1e7049
Benjamin Pang <molarmanful@users.noreply.github.com>
2015-05-07 21:31:16 -0700

    Update gcube.js

--- gcube.js
+++ gcube.js
@@ -276,22 +276,24 @@ $(document).ready(function() {
   $('head').prepend('<link rel="stylesheet" type="text/css" href="https://molarmanful.github.io/MoyuWeilong/cubenologo.css">');
 });
 $(window).load(function() {
-  $('g-cube').each(function() {
-    var s = $(this).find('g-speed').text(),
-      sc = $(this).find('g-scramble').text(),
-      a = $(this).find('g-algorithm').text(),
-      h = $(this).find('g-highlight').text(),
-      t = $(this).find('g-text').text(),
-      f = $(this).find('g-florian').text();
-    $(this).gcube().gspeed(s).gscramble(sc).galgorithm(a).ghighlight(h, function() {
-      if (ed == true) {
-        $(this).find('.cubeletId-1, .cubeletId-11, .cubeletId-19, .cubeletId-9').find('.sticker.orange').css('display', 'block');
-        console.log('Edges', ed, $(this).find('.cubeletId-1, .cubeletId-11, .cubeletId-19, .cubeletId-9').find('.sticker.orange').css('display'));
-      }
-      if (co == true) {
-        $(this).find('.cubeletId-0, .cubeletId-2, .cubeletId-20, .cubeletId-18').find('.sticker.orange').css('display', 'block');
-        console.log('Corners', co, $(this).find('.cubeletId-1, .cubeletId-11, .cubeletId-19, .cubeletId-9').find('.sticker.orange').css('display'));
-      }
-    }).gtext(t).gflorian(f);
+  $('g-cube').delegate('.sticker', 'load', function() {
+    $('g-cube').each(function() {
+      var s = $(this).find('g-speed').text(),
+        sc = $(this).find('g-scramble').text(),
+        a = $(this).find('g-algorithm').text(),
+        h = $(this).find('g-highlight').text(),
+        t = $(this).find('g-text').text(),
+        f = $(this).find('g-florian').text();
+      $(this).gcube().gspeed(s).gscramble(sc).galgorithm(a).ghighlight(h, function() {
+        if (ed == true) {
+          $(this).find('.cubeletId-1, .cubeletId-11, .cubeletId-19, .cubeletId-9').find('.sticker.orange').css('display', 'block');
+          console.log('Edges', ed, $(this).find('.cubeletId-1, .cubeletId-11, .cubeletId-19, .cubeletId-9').find('.sticker.orange').css('display'));
+        }
+        if (co == true) {
+          $(this).find('.cubeletId-0, .cubeletId-2, .cubeletId-20, .cubeletId-18').find('.sticker.orange').css('display', 'block');
+          console.log('Corners', co, $(this).find('.cubeletId-1, .cubeletId-11, .cubeletId-19, .cubeletId-9').find('.sticker.orange').css('display'));
+        }
+      }).gtext(t).gflorian(f);
+    });
   });
 });
